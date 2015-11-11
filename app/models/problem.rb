6d625df72ad141d70d40da3c262f40650314120c
SeanLi <lishuo78706@gmail.com>
2015-11-10 20:12:01 -0600

    add search form in problem section

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,12 @@
 class Problem < ActiveRecord::Base
+  #attr_accessible :problem_title, :problem_description
+ 
+  validates :problem_title, presence: true, uniqueness: true
+  validates :problem_description, presence: true
+ 
+  # It returns the articles whose titles contain one or more words that form the query	
+	def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    where("problem_title like ?", "%#{query}%") 
+  end
 end
