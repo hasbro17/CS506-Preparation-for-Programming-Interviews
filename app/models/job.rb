0f550c4d5d9abd9460d798d81382fdf7e123c235
SeanLi <lishuo78706@gmail.com>
2015-11-10 21:31:34 -0600

    add job search description and position

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,10 +2,13 @@ class Job < ActiveRecord::Base
 	has_one :job_page
 	validates :job_title, presence: true, uniqueness: true
   validates :job_description, presence: true
+  validates :job_type, presence: true
  
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
     where("job_title like ?", "%#{query}%") 
+    where("job_description like ?", "%#{query}%") 
+    where("job_type like ?", "%#{query}%") 
   end
 end
