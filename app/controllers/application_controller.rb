68075da88d32129f27f42897097fd5f374263ef4
Jacob <jwurtz12@gmail.com>
2015-11-14 02:16:51 +0000

    Implemented modify profile view and associated code in sessions_controller. Minor commenting in other files.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+#called to refresh user object
 	def authenticate_user
-		puts "authenticate_user"
 		if session[:user_id]
 			#set current user object to @current_user object variable
 			@current_user = User.find_by(username: session[:user_id])
 			return true
 		else
+			#automatically redirects user to login page on actions that require login state
 			redirect_to(:controller => 'sessions', :action => 'login')
 			return false
 		end
 	end
 
-	#def current_user
-	#	@_current_user ||= session[:current_user_id] &&  User.find_by(id: session[:current_user_id])
-	#end
+# def current_user
+#		@current_user ||= session[:current_user_id] &&  User.find_by(id: session[:current_user_id])
+#	end
 
 	def save_login_state
 		if session[:id]
