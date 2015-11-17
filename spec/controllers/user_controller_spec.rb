fe5ee8e956ec6da46b9340827f7750b01a2d25d3
witian94 <will940703@gmail.com>
2015-11-16 19:15:11 -0600

    improve user model unit test

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 
 describe "user_controller_test" do
   it "create user with controller" do
-    @newUser = User.create(
+    User.create(
         :username => "newUser",
         :email => "newUser@gmail.com",
         :password => "123456",
@@ -14,6 +14,17 @@ describe "user_controller_test" do
     user = User.find_by_username("newUser")
     expect(user).to be_truthy
     expect(user).to be_instance_of(User)
+  end
+  it "create invalid user with controller" do
+    User.create(
+        :username => "invalidUser",
+        :email => "invalidUser@gmail.com",
+        :password => "123456",
+        :password_confirmation =>"12345678",
+    )
+    user = User.find_by_username("invalidUser")
+    expect(user).to be_falsey
+    #expect(user).to be_instance_of(User)
   end
 end
 
