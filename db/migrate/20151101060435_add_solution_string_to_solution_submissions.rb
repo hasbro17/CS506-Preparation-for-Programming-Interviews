0cd5cdd640039d73818c4e2e496c5f0c27b12ea7
witian94 <will940703@gmail.com>
2015-11-16 19:17:55 -0600

    remove binary and tmp files

--- db/migrate/20151101060435_add_solution_string_to_solution_submissions.rb
+++
@@ -1,5 +0,0 @@
-class AddSolutionStringToSolutionSubmissions < ActiveRecord::Migration
-  def change
-    add_column :solution_submissions, :solution_string, :text
-  end
-end
--- db/migrate/20151112023511_add_solution_fields.rb
+++
@@ -1,8 +0,0 @@
-class AddSolutionFields < ActiveRecord::Migration
-  def change
-  	rename_column :solution_submissions, :solution_string, :generated_stdout
-    add_column :solution_submissions, :submitted_code, :text
-    add_column :solution_submissions, :stdin, :text
-    add_column :solution_submissions, :generated_errors, :text
-  end
-end
